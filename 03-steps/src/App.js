9b137cb0fcf8eb045b25f1f3dd86cdd8f4550b35
Sinthix <ivo.ivan89@hotmail.com>
2024-08-21 11:11:48 +0100

    feat: adds functionality onclick

--- 03-steps/src/App.js
+++ 03-steps/src/App.js
@@ -7,23 +7,35 @@ const messages = [
 export default function App() {
   const step = 1;
 
-  return <div className="steps">
-    <div className="numbers">
-      <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
-      <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
-      <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
+  function handlePrevious() {
+    alert('previous')
+  }
+
+  function handleNext() {
+    alert('next')
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ""}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ""}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ""}`}>3</div>
+      </div>
+
+      <p className="message">Step {step}: {messages[step -1]}</p>
+
+      <div className="buttons">
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}} 
+        onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{backgroundColor: '#7950f2', color: '#fff'}}
+        onClick={handleNext}>
+          Next
+        </button>
+      </div>
+
     </div>
-
-    <p className="message">Step {step}: {messages[step -1]}</p>
-
-    <div className="buttons">
-      <button style={{backgroundColor: '#7950f2', color: '#fff'}}>
-        Previous
-      </button>
-      <button style={{backgroundColor: '#7950f2', color: '#fff'}}>
-        Next
-      </button>
-    </div>
-
-  </div>
+  )
 }

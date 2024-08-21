c7577513876599e2e76a90335efadede4a1c779e
Sinthix <ivo.ivan89@hotmail.com>
2024-08-21 08:27:31 +0100

    fix: fixes the fragment on menu component

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -75,21 +75,22 @@ function Menu() {
 	return (
 		<main className='menu'>
 			<h2>Our Menu</h2>
-      <>
-        <p>
-          Authentic Italian cuisine. 6 creative dishes to choose from. 
-          All from our stone oven, all organic, all delicious.
-        </p>      
-      
-        {numPizzas > 0 ? (
+
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. 
+            All from our stone oven, all organic, all delicious.
+          </p>
           <ul className="pizzas">
             {pizzas.map((pizza) => <Pizza pizzaObj={pizza} key={pizza.name} />)}
           </ul>
-        ) : (
-          <p>We're still working on our menu. Please come back later :</p>
-        )}
+        </React.Fragment>
+      ) : (
+        <p>We're still working on our menu. Please come back later :</p>
+      )}
       
-      </>
+
 		</main>
 	);
 }
